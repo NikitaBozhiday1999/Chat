96f105259748f2ef3f414d7b5d80c09f66846271
Nikita <nikitaboz19@gmail.com>
2020-01-29 00:06:00 +0300

    Вот чат

---
+++ chatserve/src/main.js
@@ -0,0 +1,19 @@
+// import Vue from 'vue'
+// import { BootstrapVue } from 'bootstrap-vue';
+
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/alert';
+
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+Vue.use(BootstrapVue);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
